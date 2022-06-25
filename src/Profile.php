da3da4a59e002d09a857252cf9f3a755e7188b69
haluk <i.halukaksoy@gmail.com>
2022-06-26 00:23:31 +0300

    multiple profile creations added

--- src/Profile.php
+++ src/Profile.php
@@ -5,17 +5,31 @@ class Profile
 {
 	private $faker;
     private $gender;
+    private $numberOfProfiles;
 
     public $profile;
-	public function __construct($locale = "tr_TR", $gender = "unisex")
+	public function __construct($locale = "tr_TR", $gender = null)
 	{
         $this->faker = \Faker\Factory::create($locale);
         $this->gender = $gender;
-
-        $this->profile = new Human($this->faker, $this->gender);
+        $this->setNumberOfProfiles(1);
 	}
 
-    public function json(){
-        return json_encode($this, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+    public function createProfiles(){
+        for($i = 1; $i <= $this->numberOfProfiles; $i++){
+            if(is_null($this->gender)) {
+                $this->gender = $this->faker->randomElement(['male', 'female']);
+            }
+
+            $this->profile[] = new Human($this->faker, $this->gender);
+        }
+    }
+
+    public function getProfiles(){
+        return $this->profile;
+    }
+
+    public function setNumberOfProfiles($numberOfProfiles){
+        $this->numberOfProfiles = $numberOfProfiles;
     }
 }
